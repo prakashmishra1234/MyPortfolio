1c60ba1eb43e9008a42b1fbc29c4da0a44b02d4a
prakashmishra1234 <pm9507041@gmail.com>
2023-07-31 15:06:39 +0530

    Added points in experience

--- src/Components/Experience.tsx
+++ src/Components/Experience.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Carousel from "react-material-ui-carousel";
 import { Paper, Button, Box, Typography, Divider } from "@mui/material";
+import RadioButtonCheckedOutlinedIcon from "@mui/icons-material/RadioButtonCheckedOutlined";
 
 const Experience = () => {
   var items = [
@@ -8,19 +9,36 @@ const Experience = () => {
       Ftitle: "Vivo",
       Ltitle: "Collaboration Solutions",
       jobtitle: "Software Engineer",
-      Duration: "April, 2023 - Present",
+      Duration: "May, 2023 - Present",
+      content: [],
     },
     {
       Ftitle: "SFLHUB",
       Ltitle: "Technology",
       jobtitle: "Software Engineer",
       Duration: "July, 2022 - April, 2023",
+      content: [
+        "Manage frontend development projects from initial design through completion, optimizing all cross-browser and multi-platform compatibility.",
+        "Collaborated with 3 different backend development team on 5 new projects designs and feature improvement.",
+        "Produced multiple visual elements of web applications by translating UI/UX design wireframes into code.",
+        "Responsible for developing a responsive framework that work on smartphone as well as tablet and desktop.",
+        "Responsible for maintaining websites including content updates, debugging, feature enhancement and regular updates.",
+        "Technology used : Javascript, ReactJs, Redux, Bootstrap, Material UI, HTML, CSS.",
+      ],
     },
     {
       Ftitle: "SFLHUB",
       Ltitle: "Technology",
       jobtitle: "Software Engineer Intern",
       Duration: "January, 2022 - June, 2022",
+      content: [
+        "Analyzed source code to identify functionality issues.",
+        "Shadowed team members to learn new tasks and how to appropriately handle technical issues.",
+        "Worked with developers to identify and remove software bugs.",
+        "Partnered with team members to learn best practices in software design.",
+        "Prepared and submitted reports and other documentation to assist development team members.",
+        "Technology used : Javascript, ReactJs, Redux, Bootstrap, Material UI, HTML, CSS.",
+      ],
     },
   ];
 
@@ -36,16 +54,13 @@ const Experience = () => {
       }}
     >
       <Carousel
-        navButtonsProps={{
-          style: {
-            backgroundColor: "cornflowerblue",
-            borderRadius: 0,
-          },
+        // autoPlay={false}
+        animation="fade"
+        indicatorIconButtonProps={{
+          style: {},
         }}
-        navButtonsWrapperProps={{
+        activeIndicatorIconButtonProps={{
           style: {
-            bottom: "0",
-            top: "unset",
             color: "red",
           },
         }}
@@ -53,6 +68,7 @@ const Experience = () => {
       >
         {items.map((item, i) => (
           <Paper
+            key={i}
             elevation={3}
             sx={{
               height: "75vh",
@@ -81,6 +97,47 @@ const Experience = () => {
             >
               <Typography>{item.jobtitle}</Typography>
               <Typography>{item.Duration}</Typography>
+            </Box>
+            <Box sx={{ paddingTop: "1rem" }}>
+              {item.content.map((point, index) => {
+                return (
+                  <React.Fragment key={index}>
+                    {index === item.content.length - 1 ? (
+                      <Box
+                        sx={{
+                          my: "1rem",
+                          display: "flex",
+                        }}
+                      >
+                        <RadioButtonCheckedOutlinedIcon
+                          sx={{
+                            fontSize: "1rem",
+                            marginRight: "5px",
+                            marginTop: "5px",
+                          }}
+                        />
+                        <Typography fontWeight="700">{point}</Typography>
+                      </Box>
+                    ) : (
+                      <Box
+                        sx={{
+                          my: "1rem",
+                          display: "flex",
+                        }}
+                      >
+                        <RadioButtonCheckedOutlinedIcon
+                          sx={{
+                            fontSize: "1rem",
+                            marginRight: "5px",
+                            marginTop: "5px",
+                          }}
+                        />
+                        <Typography>{point}</Typography>
+                      </Box>
+                    )}
+                  </React.Fragment>
+                );
+              })}
             </Box>
           </Paper>
         ))}
